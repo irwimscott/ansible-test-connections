90890d47fb87d736f466a22e10abd2be89db6d3d
irwimscott <irwimscott@gmail.com>
2019-06-05 20:21:42 -0300

    Update create_varfiles.py

--- tools/create_varfiles.py
+++ tools/create_varfiles.py
@@ -1,19 +1,13 @@
 import csv
 import sys
 
-orig_stdout = sys.stdout
-f = open('output.txt', 'w')
-sys.stdout = f
-
-print('access_vars:')
 with open('list.csv') as csvfile:
     readCSV = csv.reader(csvfile, delimiter=';')
 
     for i, element in enumerate(readCSV,1):
-        print" - name: host", i
-        print"   host_src:", element[0]
-        print"   host_dst:", element[1]
-        print"   port_dst:", element[2]
-
-sys.stdout = orig_stdout
-f.close()
+        f = open("varfile_" + str(i) + ".yaml", 'w')
+        sys.stdout = f
+        print"host_src:", element[0]
+        print"host_dst:", element[1]
+        print"port_dst:", element[2]
+        f.close()
